dd158225a06f6d9b3e402a6204af2682edfccf73
Jayanthantp <jayanthantp.1993@gmail.com>
2017-03-30 04:54:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,36 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var config = {
+  host: 'http://db.imad.hasura-app.io',
+  user: 'jayanthantp',
+  password: 'db-jayanthantp-26980',
+  database: 'jayanthantp',
+  port:'5432',
+};
+
+var pool = new Pool(config);
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/test-db', function (req, res) {
+    pool.query('select * from test', function(err,result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else 
+        {
+            res.send(JSON.stringify(result));
+        }
+    })
 });
 
 app.get('/ui/madi.png', function (req, res) {
